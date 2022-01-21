d8f3f87af7373ea1f02986c919b8a1db24d438ac
KEENTO <handball0321@gmail.com>
2022-01-20 16:40:37 -0800

    Changed name of js file

--- js-files/main.js
+++ js-files/main.js
@@ -1,86 +1,10 @@
-import { header, toggleButton } from "./header.js";
-import { menuBtn, crossBtn } from "./header.js";
+const mainH1 = document.querySelector(".mainH1");
+const mainH2 = document.querySelector(".mainH2");
 
-// Get DOM
-const body = document.querySelector("body");
-body.style.height = document.body.scrollHeight + "px";
-console.log(body.style.height);
-// Back to Top Button
-const btt = document.querySelector("#backTopBtn");
-// page link triggers (links to each sections)
-const pageLinkTriggers = document.querySelectorAll(".btn-trigger");
-// global container
-const globalContainer = document.querySelector("#global-container");
-// Button switch dark and light mode (desktop ver)
-// const darkLight = document.querySelector("#darkLight");
-// differentBg (to switch the background color)
-const differentBgs = document.querySelectorAll(".differentBg");
-// x-sign9
-const xSign9s = document.querySelectorAll(".x-sign9");
-console.log(xSign9s);
-
-// Define variables
-// Javascript event on scroll up or scroll down
-// let lastScrollTop = 0;
-let isDark = true;
-
-// Build out function
-// Back to TOP from anywhere you are
-function backTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-}
-
-// Emerge back to TOP button
-function emergeButton() {
-  let pageTop = btt;
-  // 要素の位置座標を取得
-  let rect = pageTop.getBoundingClientRect();
-  // topからの距離
-  let scrollTop = rect.top + window.pageYOffset;
-
-  // btn表示開始のscrollTopの位置をレスポンシブに応じて変更する
-  let startEmerge = 690;
-  if (window.innerWidth >= 768) startEmerge = 1310;
-
-  if (scrollTop > startEmerge) {
-    pageTop.classList.add("startShow");
-  } else {
-    pageTop.classList.remove("startShow");
+export const checkHeight = () => {
+  console.log("YEEEEEEE");
+  if (window.innerHeight < 1024) {
+    mainH1.classList.add(".changeAnotherFontSize");
+    mainH2.classList.add(".changeAnotherFontSize");
   }
-}
-
-// switch dark mode and light mode
-function changeDarkLight() {
-  if (!isDark) {
-    globalContainer.classList.add("lightMode");
-    header.classList.add("lightMode");
-    // header.style.background = "rgba(254, 252, 242, 0.5)";
-    xSign9s.forEach((sign) => sign.classList.add("lightMode"));
-    differentBgs.forEach((div) => div.classList.add("lightMode"));
-  } else {
-    globalContainer.classList.remove("lightMode");
-    header.classList.remove("lightMode");
-    // header.style.background = "rgba(18, 14, 12)";
-    xSign9s.forEach((sign) => sign.classList.remove("lightMode"));
-    differentBgs.forEach((div) => div.classList.remove("lightMode"));
-  }
-  isDark = !isDark;
-}
-
-// Hook up the event
-menuBtn.addEventListener("click", toggleButton);
-crossBtn.addEventListener("click", toggleButton);
-
-btt.addEventListener("click", backTop);
-window.addEventListener("scroll", emergeButton);
-
-pageLinkTriggers.forEach((trigger) =>
-  trigger.addEventListener("click", toggleButton)
-);
-
-// darkLight.addEventListener("click", changeDarkLight);
-
-AOS.init();
+};
