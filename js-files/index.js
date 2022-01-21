cb6f72b9ebccfb93c62972fc85b2c40ebea6c61e
KEENTO <handball0321@gmail.com>
2022-01-21 15:06:39 -0800

    Changed the font-family of workTitle

--- js-files/index.js
+++ js-files/index.js
@@ -7,7 +7,7 @@ const body = document.querySelector("body");
 body.style.height = document.body.scrollHeight + "px";
 console.log(body.style.height);
 // Back to Top Button
-const btt = document.querySelector("#backTopBtn");
+// const btt = document.querySelector("#backTopBtn");
 // page link triggers (links to each sections)
 const pageLinkTriggers = document.querySelectorAll(".btn-trigger");
 // global container
@@ -35,23 +35,23 @@ function backTop() {
 }
 
 // Emerge back to TOP button
-function emergeButton() {
-  let pageTop = btt;
-  // 要素の位置座標を取得
-  let rect = pageTop.getBoundingClientRect();
-  // topからの距離
-  let scrollTop = rect.top + window.pageYOffset;
+// function emergeButton() {
+//   let pageTop = btt;
+//   // 要素の位置座標を取得
+//   let rect = pageTop.getBoundingClientRect();
+//   // topからの距離
+//   let scrollTop = rect.top + window.pageYOffset;
 
-  // btn表示開始のscrollTopの位置をレスポンシブに応じて変更する
-  let startEmerge = 690;
-  if (window.innerWidth >= 768) startEmerge = 1310;
+//   // btn表示開始のscrollTopの位置をレスポンシブに応じて変更する
+//   let startEmerge = 690;
+//   if (window.innerWidth >= 768) startEmerge = 1310;
 
-  if (scrollTop > startEmerge) {
-    pageTop.classList.add("startShow");
-  } else {
-    pageTop.classList.remove("startShow");
-  }
-}
+//   if (scrollTop > startEmerge) {
+//     pageTop.classList.add("startShow");
+//   } else {
+//     pageTop.classList.remove("startShow");
+//   }
+// }
 
 // switch dark mode and light mode
 function changeDarkLight() {
@@ -76,7 +76,7 @@ menuBtn.addEventListener("click", toggleButton);
 crossBtn.addEventListener("click", toggleButton);
 
 // btt.addEventListener("click", backTop);
-window.addEventListener("scroll", emergeButton);
+// window.addEventListener("scroll", emergeButton);
 
 pageLinkTriggers.forEach((trigger) =>
   trigger.addEventListener("click", toggleButton)
