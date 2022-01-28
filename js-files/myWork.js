82788893c3b0764517950dff1921a7e200fbe272
KEENTO <handball0321@gmail.com>
2022-01-28 14:43:13 -0800

    Add things into the head of myWork-third.html

--- js-files/myWork.js
+++ js-files/myWork.js
@@ -1,2 +1,8 @@
 import { header, toggleButton } from "./header.js";
 import { menuBtn, crossBtn } from "./header.js";
+
+// Hook up the event
+menuBtn.addEventListener("click", toggleButton);
+crossBtn.addEventListener("click", toggleButton);
+
+AOS.init();
