ad546db9e14149979d3396bd31cb5d912f385f0a
KEENTO <handball0321@gmail.com>
2022-01-27 22:22:32 -0800

    Add myWork.js file

--- js-files/main.js
+++ js-files/main.js
@@ -2,7 +2,6 @@ const mainH1 = document.querySelector(".mainH1");
 const mainH2 = document.querySelector(".mainH2");
 
 export const checkHeight = () => {
-  console.log("YEEEEEEE");
   if (window.innerHeight < 1024) {
     mainH1.classList.add(".changeAnotherFontSize");
     mainH2.classList.add(".changeAnotherFontSize");
---
+++ js-files/myWork.js
@@ -0,0 +1,2 @@
+import { header, toggleButton } from "./header.js";
+import { menuBtn, crossBtn } from "./header.js";
